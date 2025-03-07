cffa1cd6abb3cf775e5e5ecec5a883a0cc0e97dd
mahaputra2512 <97659502+mahaputra2512@users.noreply.github.com>
2025-03-07 10:14:31 +0700

    Update testlogin.py

--- testlogin.py
+++ testlogin.py
@@ -2,14 +2,20 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from selenium.webdriver.firefox.options import Options
 
 # Fungsi untuk menguji login
 def test_login(username, password, expected_message):
+    # Konfigurasi Firefox Options
+    options = Options()
+    options.add_argument('--headless')  # Menjalankan browser di mode headless
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
+
     # Inisialisasi driver Firefox menggunakan Remote WebDriver
     driver = webdriver.Remote(
         command_executor='http://localhost:4444/wd/hub',
-        desired_capabilities=DesiredCapabilities.FIREFOX
+        options=options
     )
     driver.get("http://localhost:3000/login.php")
     
